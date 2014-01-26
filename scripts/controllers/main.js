4617577509095acc1854ed96bf9d01b15bcca9d6
manviny <manol@mac.home>
2014-01-26 17:01:14 +0100

    First commit

---
+++ scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('patrimonio24AppApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
